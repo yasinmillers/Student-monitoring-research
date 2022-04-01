75677ed16ea6e9ccad5006e171618f9ed54f6ac9
yasinmillers <ssenyonga100@gmail.com>
2022-04-01 17:18:46 +0300

    second

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ if(isset($_REQUEST['sub']))
 $a = $_REQUEST['name'];
 $b = $_REQUEST['password'];
 
-$res = mysqli_query($cser,"select* from students where name='$a'and password='$b'");
+$res = mysqli_query($cser,"select* from students where name='$name'and password='$password'");
 $result=mysqli_fetch_array($res);
 if($result)
 {

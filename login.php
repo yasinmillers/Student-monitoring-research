f85b6013fe45aae090dbfa1432b50707ff103d34
yasinmillers <ssenyonga100@gmail.com>
2022-04-01 17:34:03 +0300

    login

--- login.php
+++ login.php
@@ -46,10 +46,10 @@ include("connect.php");
 
 if(isset($_REQUEST['sub']))
 {
-$a = $_REQUEST['name'];
+$a = $_REQUEST['email'];
 $b = $_REQUEST['password'];
 
-$res = mysqli_query($cser,"select* from students where name='$name'and password='$password'");
+$res = mysqli_query($cser,"select* from students where email='$a'and password='$b'");
 $result=mysqli_fetch_array($res);
 if($result)
 {

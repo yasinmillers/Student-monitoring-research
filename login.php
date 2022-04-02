3665e3bb0a76e5c3c8be8c3f7ccf6db518b3634e
yasinmillers <ssenyonga100@gmail.com>
2022-04-02 10:32:59 +0300

    hello today

--- login.php
+++ login.php
@@ -8,25 +8,29 @@
 session_start ();
 include("connect.php"); 
 
-if(isset($_REQUEST['sub']))
-{
-$a = $_REQUEST['email'];
-$b = $_REQUEST['password'];
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
+// if(isset($_REQUEST['sub']))
+// {
+// $a = $_REQUEST['email'];
+// $b = $_REQUEST['password'];
 
-$res = mysqli_query($cser,"select* from students where email='$a'and password='$b'");
-$result=mysqli_fetch_array($res);
-if($result)
-{
+// $res = mysqli_query($cser,"select* from students where email='$a'and password='$b'");
+// $result=mysqli_fetch_array($res);
+// if($result)
+// {
 	
-	$_SESSION["login"]="1";
-	header("location:index.php");
-}
-else	
-{
-	header("location:login.php?err=1");
+// 	$_SESSION["login"]="1";
+// 	header("location:index.php");
+// }
+// else	
+// {
+// 	header("location:login.php?err=1");
 	
-}
-}
+// }
+// }
 ?>
 
 <!DOCTYPE html>

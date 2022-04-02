16ee07ddbbe2dc29923ab3d6051fb0f7ce29a5e8
yasinmillers <ssenyonga100@gmail.com>
2022-04-02 10:51:42 +0300

    aa

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 <?php include 'partials/nav.php'; ?>
 <?php include 'connect.php'; ?>
 
-<?php
 
+<?php
 session_start ();
 include("connect.php"); 
 
@@ -21,25 +21,24 @@ echo $msg;
 ?>
 
 </p>
-// if(isset($_REQUEST['sub']))
-// {
-// $a = $_REQUEST['email'];
-// $b = $_REQUEST['password'];
+<?php
+if(isset($_REQUEST['sub']))
+ {
+ $a = $_REQUEST['email'];
+ $b = $_REQUEST['password'];
 
-// $res = mysqli_query($cser,"select* from students where email='$a'and password='$b'");
-// $result=mysqli_fetch_array($res);
-// if($result)
-// {
+ $res = mysqli_query($cser,"select* from students where email='$a'and password='$b'");
+ $result=mysqli_fetch_array($res);
+ if($result)
+ {
+		$_SESSION["login"]="1";
+ 	header("location:index.php");
+ }
+ else	
+ {
+ 	header("location:login.php?err=1");
 	
-// 	$_SESSION["login"]="1";
-// 	header("location:index.php");
-// }
-// else	
-// {
-// 	header("location:login.php?err=1");
-	
-// }
-// }
+ } }
 ?>
 
 <!DOCTYPE html>

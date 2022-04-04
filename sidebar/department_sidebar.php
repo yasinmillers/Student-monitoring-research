a281390759439ec3928be3cd076702679494e1cf
yasinmillers <ssenyonga100@gmail.com>
2022-04-04 18:06:51 +0300

    ffff

---
+++ sidebar/department_sidebar.php
@@ -0,0 +1,38 @@
+     <div class="span3" id="sidebar">
+                    <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+                        <li> <a href="dashboard.php"><i class="icon-chevron-right"></i><i class="icon-home"></i>&nbsp;Dashboard</a> </li>
+						<li>
+                            <a href="subjects.php"><i class="icon-chevron-right"></i><i class="icon-list-alt"></i> Subject</a>
+                        </li>
+						<li>
+                            <a href="class.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Group</a>
+                        </li>
+						<li>
+                            <a href="admin_user.php"><i class="icon-chevron-right"></i><i class="icon-user"></i> Admin </a>
+                        </li>
+						<li  class="active">
+                            <a href="department.php"><i class="icon-chevron-right"></i><i class="icon-building"></i> Department</a>
+                        </li>
+						<li>
+                            <a href="students.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Supervisor</a>
+                        </li>
+						<li>
+                            <a href="teachers.php"><i class="icon-chevron-right"></i><i class="icon-group"></i>Students</a>
+                        </li>
+						<li>
+                            <a href="downloadable.php"><i class="icon-chevron-right"></i><i class="icon-download"></i> Downloadable Materials</a>
+                        </li>
+						<li>
+                            <a href="assignment.php"><i class="icon-chevron-right"></i><i class="icon-upload"></i> Uploaded Assignments</a>
+                        </li>
+						
+						
+						
+						<li>
+                            <a href="school_year.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i> School Year</a>
+                        </li>
+                    </ul>
+					
+					
+				
+                </div>
